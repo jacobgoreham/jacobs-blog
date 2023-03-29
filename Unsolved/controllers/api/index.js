ab57903e6ac7c5be3b6adaf2c59233f39f5969ae
Jacob Goreham <jacobgoreham@gmail.com>
2023-03-28 21:21:07 -0700

    get every post get post by id and create new post routes

--- Unsolved/controllers/api/index.js
+++ Unsolved/controllers/api/index.js
@@ -1,6 +1,13 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
 
-router.use('/users', userRoutes);
+const userRoutes = require("./user-routes.js");
+const postRoutes = require("./post-routes.js");
+const commentRoutes = require("./comment-routes.js");
 
+//routes for users posts comments
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
+
+//export
 module.exports = router;
---
+++ Unsolved/controllers/api/postRoutes.js
@@ -0,0 +1,86 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+//get every post
+router.get("/", (req, res) => {
+  //finds all post
+  Post.findAll({
+    attributes: ["id", "content", "title", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      //comment model
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//get post by ID
+router.get("/:id", (req, res) => {
+  //grabs post by req.params.id
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    //if 404 on postData response
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post with this ID...",
+        });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//create a new post
+router.post("/", withAuth, (req, res) => {
+  console.log("Creating Post...");
+  Post.create({
+    title: req.body.title,
+    content: req.body.post_content,
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
