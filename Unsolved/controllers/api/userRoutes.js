80d69cfbfbc36926bbb604f85ae303036cc2cd49
Jacob Goreham <jacobgoreham@gmail.com>
2023-03-28 22:15:27 -0700

    finished userRoutes

--- Unsolved/controllers/api/userRoutes.js
+++ Unsolved/controllers/api/userRoutes.js
@@ -1,43 +1,138 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Comment } = require("../../models");
 
-router.post("/login", async (req, res) => {
-  try {
-    // TODO: Finding User that email field matches the body prop
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
-    }
-
-    // TODO: Chcking that password matches the one in the database
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
-    }
-
-    // TODO: Creating a session variable based on user being logged in
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.json({ user: userData, message: "You are now logged in!" });
+//get every user
+router.get("/", (req, res) => {
+  //grabs all users
+  User.findAll({
+    attributes: {
+      exclude: ["password"],
+    },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  } catch (err) {
-    res.status(400).json(err);
-  }
 });
 
+//route for getting user by ID
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: {
+      exclude: ["password"],
+    },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "content", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  })
+    //if no user data returns
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({
+          message: "No post with this ID...",
+        });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//create new user with username and password
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+  })
+    //session saves user with an id, username, and that logged in is set to true
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//route for login
+router.post("/login", (req, res) => {
+  //finds user by req.body.username
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+    //if theres not a user found shows err
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({
+        message: "No user with that username...",
+      });
+      return;
+    }
+    //no err => session logs in UserData.id and .username
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({
+        user: dbUserData,
+        message: "You are logged in!",
+      });
+    });
+
+    //password check
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({
+        message: "Incorrect password!",
+      });
+      return;
+    }
+
+    //if password check is a go then session saves UserData
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+      //and logs in
+      res.json({
+        user: dbUserData,
+        message: "You are logged in!",
+      });
+    });
+  });
+});
+
+//logout route
 router.post("/logout", (req, res) => {
-  if (req.session.logged_in) {
-    // TODO: Session is destroyed when logging out.
+  //when logout is ran session will destroy the loggedIn with res.status(204).end();, on 404 error will .end();
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
