2cd22dc6cb38d48d393dc980729a48eb0f8ea9e0
Jacob Goreham <jacobgoreham@gmail.com>
2023-03-28 22:25:52 -0700

    fixed auth, added handlebars files ready to use

--- Unsolved/utils/auth.js
+++ Unsolved/utils/auth.js
@@ -2,7 +2,7 @@ const withAuth = (req, res, next) => {
   // TODO: Add a comment describing the functionality of this if statement
   //if users not logged in this will send them to our login page
   // TODO: If user not logged in then is sent to login page.
-  if (!req.session.logged_in) {
+  if (!req.session.user_in) {
     res.redirect("/login");
   } else {
     next();
