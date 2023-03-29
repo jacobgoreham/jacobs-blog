94805706b6678beda6799edc653fc69a51c0d7a7
Jacob Goreham <jacobgoreham@gmail.com>
2023-03-28 20:57:06 -0700

    finished commentRoutes

--- Unsolved/controllers/api/commentRoutes.js
+++ Unsolved/controllers/api/commentRoutes.js
@@ -11,3 +11,24 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+//create a new comment
+router.post("/", withAuth, (req, res) => {
+  if (req.session) {
+    //comment model
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    })
+      //error
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+//export
+module.exports = router;
