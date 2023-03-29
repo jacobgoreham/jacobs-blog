8d673589103ee5d306537aebbac3530eec41ff2a
Jacob Goreham <jacobgoreham@gmail.com>
2023-03-28 20:38:55 -0700

    Finished home routes

--- Unsolved/controllers/homeRoutes.js
+++ Unsolved/controllers/homeRoutes.js
@@ -1,35 +1,116 @@
 const router = require("express").Router();
-const { User } = require("../models");
-const withAuth = require("../utils/auth");
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
 
-// TODO: Finds user who matches the posted email address
-router.get("/", withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
+//landing page for homeRoutes
+router.get("/", (req, res) => {
+  //grabs all posts no login needed*
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    //limits post data and renders into homepage
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) =>
+        post.get({
+          plain: true,
+        })
+      );
+
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render("homepage", {
-      users,
-      // TODO: Passes the loggedIn flag to our handlebar template
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
+//route for grabbing specific post by id
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    //if 404 on postData response
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post with this ID...",
+        });
+        return;
+      }
+
+      const post = dbPostData.get({
+        plain: true,
+      });
+
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//login route if authorized
 router.get("/login", (req, res) => {
-  // TODO: If user succesfully logged in is sent to the homepage
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     res.redirect("/");
     return;
   }
 
   res.render("login");
+});
+
+//route for signup sheet and if the session is logged in we're redirected to the home landing page
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+//any errors response gets error 404
+router.get("*", (req, res) => {
+  res.status(404).send("Error 404!");
+  res.redirect("/");
 });
 
 module.exports = router;
